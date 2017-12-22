c6fb105569eeec2317ccbc614d577ba033b71f56
Elecia White <elecia@logicalelegance.com>
2017-12-22 00:42:57 +0000

    kvm function does most of what I want with respect to identifying key in new image and locating it in canonical image

--- prototyper/TyKey.py
+++ prototyper/TyKey.py
@@ -4,6 +4,102 @@ from matplotlib import pyplot as plt
 import math
 
 
+class KeyMap():
+    def __init__(self, cam, debugging = 0):
+        self.cam = cam
+        self.debugging = debugging
+        self.keyImg = cv2.imread("keys.png")
+        self.cameraImageName = 'Ty View'
+        self.clickEvent = False
+
+    def mouseClickCallback(self, event, x, y, flags, params):
+        if event == cv2.EVENT_LBUTTONDBLCLK:
+            self.clickEvent = True
+            self.click_x = x
+            self.click_y = y
+
+    def transformQtoT(self, M, q):
+        a = np.array([[q]], dtype='float32')
+        return cv2.perspectiveTransform(a,M)
+
+    def transformTtoQ(self, M, t):
+        a = np.array([[t]], dtype='float32')
+        return cv2.perspectiveTransform(a,np.linalg.inv(M))
+        
+
+    def kvm(self):
+        self.clickEvent = False
+        ret, frame = self.cam.read()
+        cv2.imshow(self.cameraImageName, frame)
+        resp = cv2.waitKey(20) & 0xFF
+        cv2.setMouseCallback(self.cameraImageName, self.mouseClickCallback)
+
+        while self.clickEvent == False and resp != ord('q'):
+            ret, frame = self.cam.read()
+            cv2.imshow(self.cameraImageName, frame)
+            self.img = frame
+            resp = cv2.waitKey(20) & 0xFF
+        if self.clickEvent == True:
+            dst,M = self.showFlannH(self.keyImg,frame)
+            out = self.transformTtoQ(M, (self.click_x,self.click_y))
+            print out
+  
+
+        cv2.destroyAllWindows()
+    
+    def showFlannH(self, img1, img2):
+        MIN_MATCH_COUNT = 10
+        # Initiate SIFT detector
+        sift = cv2.xfeatures2d.SIFT_create()
+
+        # find the keypoints and descriptors with SIFT
+        kp1, des1 = sift.detectAndCompute(img1,None)
+        kp2, des2 = sift.detectAndCompute(img2,None)
+
+
+        FLANN_INDEX_KDTREE = 0
+        index_params = dict(algorithm = FLANN_INDEX_KDTREE, trees = 5)
+        search_params = dict(checks = 50)
+
+        flann = cv2.FlannBasedMatcher(index_params, search_params)
+
+        matches = flann.knnMatch(des1,des2,k=2)
+
+        # store all the good matches as per Lowe's ratio test.
+        good = []
+        for m,n in matches:
+            if m.distance < 0.7*n.distance:
+                good.append(m)
+
+        if len(good)>MIN_MATCH_COUNT:
+            src_pts = np.float32([ kp1[m.queryIdx].pt for m in good ]).reshape(-1,1,2)
+            dst_pts = np.float32([ kp2[m.trainIdx].pt for m in good ]).reshape(-1,1,2)
+
+            M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
+            matchesMask = mask.ravel().tolist()
+
+            h,w = img1.shape[0:2]
+            pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
+            dst = cv2.perspectiveTransform(pts,M)
+
+            img2 = cv2.polylines(img2,[np.int32(dst)],True,255,3, cv2.LINE_AA)
+
+        else:
+            print "Not enough matches are found - %d/%d" % (len(good),MIN_MATCH_COUNT)
+            matchesMask = None
+
+        draw_params = dict(matchColor = (0,255,0), # draw matches in green color
+                       singlePointColor = None,
+                       matchesMask = matchesMask, # draw only inliers
+                       flags = 2)
+
+        img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
+
+        plt.imshow(img3, 'gray'),plt.show()
+        return dst, M
+
+
+
 def getCam():
     videoChannel = 1
     cam = cv2.VideoCapture(videoChannel)
@@ -67,14 +163,18 @@ def medianAt(idx, inputlist):
 def flannInfo(kp_query, kp_train, matches):
     i =0
     out = []
+    qpts = []
+    tpts = []
     for mn in matches:
         m = mn[0]
         q = kp_query[m.queryIdx]
         t = kp_train[m.trainIdx]
         xoff = q.pt[0] - t.pt[0]
         yoff = q.pt[1] - t.pt[1]
-        out.append((m.distance,  xoff, yoff, distance(q.pt, t.pt), angle(q.pt, t.pt))) 
-    return out       
+        out.append((m.distance, xoff, yoff, distance(q.pt, t.pt), angle(q.pt, t.pt))) 
+        qpts.append(q.pt) 
+        tpts.append(t.pt) 
+    return out, qpts, tpts
 
 
 def showFlann(queryImage, trainingImage, threshold=0.7):
@@ -120,7 +220,7 @@ def showFlannH(img1, img2):
         M, mask = cv2.findHomography(src_pts, dst_pts, cv2.RANSAC,5.0)
         matchesMask = mask.ravel().tolist()
 
-        h,w = img1.shape
+        h,w = img1.shape[0:2]
         pts = np.float32([ [0,0],[0,h-1],[w-1,h-1],[w-1,0] ]).reshape(-1,1,2)
         dst = cv2.perspectiveTransform(pts,M)
 
@@ -131,13 +231,14 @@ def showFlannH(img1, img2):
         matchesMask = None
 
     draw_params = dict(matchColor = (0,255,0), # draw matches in green color
-                       singlePointColor = None,
-                       matchesMask = matchesMask, # draw only inliers
-                       flags = 2)
+                   singlePointColor = None,
+                   matchesMask = matchesMask, # draw only inliers
+                   flags = 2)
 
     img3 = cv2.drawMatches(img1,kp1,img2,kp2,good,None,**draw_params)
 
     plt.imshow(img3, 'gray'),plt.show()
+    return dst, M
 
 
 
